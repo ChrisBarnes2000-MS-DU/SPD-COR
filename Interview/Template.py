1865d46688b9ef7b32927ca08e0a2fc7dde8b31e
Christopher Barnes <chris.barnes.2000@me.com>
2020-07-29 19:51:56 -0700

    [add] Interview problems

---
+++ Interview/Template.py
@@ -0,0 +1,34 @@
+"""Problem: 
+    -
+
+    Example Input:
+        -
+
+    Example Output:
+        -
+
+    Assumptions:
+        -
+
+"""
+
+"""Pseudo Approach
+    -
+
+    Edge Cases:
+        -
+
+    Complexity Check:
+        After implementing some code go back through and revaluate its time and/or space complexity -- refractor/improve/find more edge cases -- repeat
+"""
+
+class ClassName:
+    def function_name(self, param):
+        function_return = None
+        return function_return
+
+if __name__ == "__main__":                              # RunTime:
+    c = ClassName()
+    param = None
+    function_output = c.function_name(param)
+    print(function_output)
